484365f7205e478e0c840b5a5f0600b5eb2f67f4
Guirecam <guillerc18@gmail.com>
2025-12-22 17:20:34 +0100

    Mostrar todos los proyectos del JSON en la página

--- script.js
+++ script.js
@@ -3,8 +3,24 @@ console.log('funcionando')
 fetch("projects.json")
     .then((response) => response.json())
     .then((projects) => {
-        console.log("Proyectos cargados del JSON", projects);
+        const container = document.getElementById("projects");
+        projects.forEach(project => {
+            const card = document.createElement("article");
+            card.classList.add("project-card");
+            card.innerHTML = `
+    <h2>${project.title}</h2>
+    <p>${project.description}</p>
+    <p><strong>Estado:</strong> ${project.status}</p>
+    <p><strong>Categoría:</strong> ${project.category}</p>
+    <div class="progress-bar">
+      <div class="progress-fill" style="width: ${project.progress}%"></div>
+    </div>
+    <p><strong>Siguiente paso:</strong> ${project.nextStep}</p>
+  `;
+            container.appendChild(card);
+        });
     })
+    
     .catch((error) => {
         console.error("Error cargando projects.json", error);
     });
